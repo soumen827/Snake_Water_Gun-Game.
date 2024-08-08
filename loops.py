acda16ffb7e531e6ba29898b71aefd494815d4b4
soumen827 <soumenlaha465@gmail.com>
2024-08-08 22:27:05 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,20 @@
+n=int(input("Enter your no:"))
+product =1
+for i in range (1,n+1):
+    product = product * i
+print(f"The factoreal of {n} is {product}")
+    
+  
+   
+
+  
+    
+
+
+    
+
+
+
+
+
+    
