eb63c559d6cedb172a007e13e03e89d8bd61c9bd
soumen827 <soumenlaha465@gmail.com>
2024-08-09 22:32:31 +0530

    Add files via upload

--- 2nd.py
+++ 2nd.py
@@ -1,8 +1,12 @@
-n= int(input("Enter your number: "))
-for i in range (1 , n+1):
-    print(" "*(n-i), end="")
-    print("*"*(2*i-1), end="")
-    print(" ")
+n=int(input("Enter your name:"))
+for i in range(1,n+1):
+    if(i==1 or i==n):
+        print("*"*n,end="")
+    else:
+        print("*",end="")
+        print(" "*(n-2),end="")
+        print("*",end="")
+    print()
 
 
 
